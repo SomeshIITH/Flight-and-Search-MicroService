3e8bb9a2f66b3f92d9fa321a0d4b3d11fb5decbe
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-04 14:50:41 +0530

    undo migrationf anfd put constraint  that city name will be unique

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig.js');
+// const {City} = require('./models/index.js');
 
 const SetupServer = async () => {
     const app = express();
@@ -8,8 +9,9 @@ const SetupServer = async () => {
     app.use(bodyParser.json());
 
 
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(PORT);
+        // console.log(City);
         console.log(`Server is running on http://localhost:${PORT}`);
     })
 }
