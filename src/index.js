35258ae6269e217e8cc2dc4b7bbb80c52b73b161
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-04 12:51:32 +0530

    basic server is ready , next step is to set sequelize

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
-
+const {PORT} = require('./config/serverConfig.js');
 
 const SetupServer = async () => {
     const app = express();
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
-    app.listen(PORT,()=>{
+
+    app.listen(PORT,() => {
         console.log(PORT);
         console.log(`Server is running on http://localhost:${PORT}`);
     })
