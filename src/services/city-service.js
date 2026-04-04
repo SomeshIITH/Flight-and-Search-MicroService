f9526b6dee7a6629d7277209c7d29c3e19a2ab15
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-04 15:36:07 +0530

    city service logic added

---
+++ src/services/city-service.js
@@ -0,0 +1,69 @@
+const {CityRepository} = require('./../repository/index.js');
+
+class CityService{
+    constructor(){
+        this.cityRepo = new CityRepository();
+    }
+
+    async createCity(data){
+        try{
+            const city = await this.cityRepo.createCity(data);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in city service layer");
+            throw {error};
+        }
+    }
+
+    async createMultipleCities(data){
+        try{
+            const cities = await this.cityRepo.createMultipleCities(data);
+            return cities;
+        }catch(error){
+            console.log("Something went wrong in city service layer");
+            throw {error};
+        }
+    }
+
+    async getCityById(id){
+        try{
+            const city = await this.cityRepo.getCityById(id);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in city service layer");
+            throw {error};
+        }
+    }
+
+    async getCityByFilter(filter){
+        try{
+            const city = await this.cityRepo.getCityByFilter(filter);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in city service layer");
+            throw {error};
+        }
+    }
+
+    async updateCityById(id,data){
+        try{
+            const city = await this.cityRepo.updateCityById(id,data);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in city service layer");
+            throw {error};
+        }
+    }
+
+    async destroyCityById(id){
+        try{
+            const city = await this.cityRepo.destroyCityById(id);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in city service layer");
+            throw {error};
+        }
+    }
+};
+
+module.exports = CityService;
