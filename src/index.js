a3a84683de643ffba0ca66f3616ad84236202670
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-04 12:35:50 +0530

    boiler code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('bidy-parser');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+const SetupServer = async () => {
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.listen(PORT,()=>{
+        console.log(PORT);
+        console.log(`Server is running on http://localhost:${PORT}`);
+    })
+}
+
+SetupServer();
